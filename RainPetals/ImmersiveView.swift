47895412cc141b9a6a4785402eda83f45d35b237
Sam Hodak <slhodak@gmail.com>
2024-03-05 17:13:27 -0800

    simplify

--- RainPetals/ImmersiveView.swift
+++ RainPetals/ImmersiveView.swift
@@ -13,21 +13,19 @@ import DequeModule
 
 struct ImmersiveView: View {
     @State var rootEntity = Entity()
-    @State var rainDrops = Deque<ModelEntity>()
-    let rainTimer = Timer.publish(every: 0.005, on: .main, in: .common).autoconnect()
+    let rainTimer = Timer.publish(every: 0.002, on: .main, in: .common).autoconnect()
     @State var collisionSubscription: EventSubscription?
     
     var body: some View {
         RealityView { content in
             rootEntity.addChild(ARSession.shared.sessionRootEntity)
+            content.add(rootEntity)
             
             collisionSubscription = content.subscribe(to: CollisionEvents.Began.self) { ce in
                 Task {
                     await self.handleCollision(ce)
                 }
             }
-            
-            content.add(rootEntity)
         }
         .task {
             await runARSession()
@@ -37,6 +35,7 @@ struct ImmersiveView: View {
         }
         .onReceive(rainTimer, perform: { _ in
             spawnRaindrop()
+            
         })
     }
     
@@ -51,62 +50,66 @@ struct ImmersiveView: View {
         }
     }
     
+    @MainActor
     func spawnRaindrop() {
         // Spawn physical raindrops from a random point in a given area
-        let radius: Float = 0.003
+        let radius: Float = 0.002
         let mesh = MeshResource.generateSphere(radius: radius)
         let shape = ShapeResource.generateSphere(radius: radius)
         var raindropMaterial = PhysicallyBasedMaterial()
         raindropMaterial.baseColor = .init(tint: .white)
         raindropMaterial.roughness = 0.0
         raindropMaterial.metallic = 0.1
+        raindropMaterial.blending = .transparent(opacity: 0.0)
         let rainDrop = ModelEntity(mesh: mesh,
                                    materials: [raindropMaterial])
-        rainDrop.components.set(OpacityComponent(opacity: 0.2))
         rainDrop.components.set(CollisionComponent(shapes: [shape]))
-        rainDrop.components.set(PhysicsBodyComponent())
-        rainDrop.position = [Float.random(in: -3...3),
+        var physicsComponent = PhysicsBodyComponent()
+        physicsComponent.linearDamping = 5
+        rainDrop.components.set(physicsComponent)
+        rainDrop.position = [Float.random(in: -1...1),
                              Float.random(in: 2...2.5),
-                             Float.random(in: (-2)...(-1))]
+                             Float.random(in: (-2)...(0))]
         rainDrop.name = "RainDrop"
-        rainDrops.append(rainDrop)
-        if rainDrops.count > 500 {
-            if let oldRainDrop = rainDrops.popFirst() {
-                rootEntity.removeChild(oldRainDrop)
-            }
-        }
         rootEntity.addChild(rainDrop)
     }
     
     @MainActor
     func handleCollision(_ ce: CollisionEvents.Began) async {
-        var rainDropEntity: Entity? = nil
+        var raindropEntity: Entity? = nil
         if ce.entityA.name.contains("RainDrop") && !ce.entityB.name.contains("RainDrop") {
-            rainDropEntity = ce.entityA
+            raindropEntity = ce.entityA
         } else if !ce.entityA.name.contains("RainDrop") && ce.entityB.name.contains("RainDrop") {
-            rainDropEntity = ce.entityB
+            raindropEntity = ce.entityB
         } else if ce.entityA.name.contains("RainDrop") && ce.entityB.name.contains("RainDrop") {
             // TODO: combine into puddle
         }
         
-        if let rainDropEntity = rainDropEntity {
-            print("raindrops hit a surface")
-            // Maybe turn into a petal
-            let turnIntoPetal = Float.random(in: 0...1) > 0.5
-            if turnIntoPetal {
-                // a. get entity location and place a new flowerpetal entity there
-                // b. replace entity mesh with flowerpetal model
-                do {
-                    let randomPetal = Int.random(in: 1...9)
-                    let petalEntity = try await Entity(named: "rosePetals\(randomPetal)", in: realityKitContentBundle)
-                    petalEntity.position = rainDropEntity.position
-                    rootEntity.removeChild(rainDropEntity)
-                    rootEntity.addChild(petalEntity)
-                } catch {
-                    print("Error loading rose petal")
-                    print(error)
-                }
+        guard let raindropEntity = raindropEntity else { return }
+        
+        let turnIntoPetal = Float.random(in: 0...1) > 0.2
+        if turnIntoPetal {
+            await spawnPetal(for: raindropEntity)
+        } else {
+            rootEntity.removeChild(raindropEntity)
+        }
+    }
+    
+    @MainActor
+    func spawnPetal(for rainDropEntity: Entity) async {
+        do {
+            let randomPetal = Int.random(in: 1...9)
+            let petalEntity = try await Entity(named: "rosePetals\(randomPetal)", in: realityKitContentBundle)
+            // Prevents unnatural bouncing of petals -- but other methods to dampen physics would be better
+            if let petalModelEntity = petalEntity.findEntity(named: "Mesh") {
+                petalModelEntity.components[PhysicsBodyComponent.self]?.isTranslationLocked = (x: true, y: true, z: true)
+                petalModelEntity.components[PhysicsBodyComponent.self]?.isRotationLocked = (x: true, y: true, z: true)
             }
+            petalEntity.position = rainDropEntity.position
+            rootEntity.addChild(petalEntity)
+        } catch {
+            print("Error loading rose petal")
+            print(error)
         }
     }
 }
